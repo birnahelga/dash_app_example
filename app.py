96b7db44ef0b1811b529a963ae5981c2e8cfb848
Birna Helga <birnahelga.johannesdottir@alumni.esade.edu>
2018-12-19 14:16:26 +0100

    Commit

--- app.py
+++ app.py
@@ -27,20 +27,35 @@ df1 = pd.read_csv('nama_10_gdp_1_Data.csv',na_values=':')
 #remove European Union and Euro area
 df = df1[~df1['GEO'].str.contains('Euro')]
 
-#Use for graph 2, only units that are in current prices, million euro
-dff = df[df['UNIT'] == 'Current prices, million euro']
+#rename Germany, Kosovo and FYR Macedonia
+df= df.replace({'Germany (until 1990 former territory of the FRG)': 'Germany',
+                'Kosovo (under United Nations Security Council Resolution 1244/99)': 'Kosovo',
+                'Former Yugoslav Republic of Macedonia, the': 'FYR Maceodonia'
+               })
 
-
-available_indicators = df['NA_ITEM'].unique()
-available_geo=df['GEO'].unique()
-available_unit=df['UNIT'].unique()
+available_indicators = sorted(df['NA_ITEM'].unique())
+available_geo=sorted(df['GEO'].unique())
+available_unit=sorted(df['UNIT'].unique())
 available_year=df['TIME'].unique()
 
 
-app.layout = html.Div([
+app.layout = html.Div(style={'backgroundColor': '#F5E6F6'},children=[
+    
+    html.H1('Final Project Dash - Birna Helga', 
+            style={
+            'textAlign': 'center',
+            'color': 'darkviolet'
+        }),
+    
+    html.H4('Graph 1: Scatterplot', 
+            style={
+            'color': 'darkviolet'
+        } ),
+
+    
     #First graph
     html.Div([
-
+        
         html.Div([
             dcc.Dropdown(
                 id='xaxis-column1',
@@ -55,13 +70,13 @@ app.layout = html.Div([
             )
                     
         ],
-        style={'width': '40%','padding':20, 'display': 'inline-block'}),
+        style={'width': '45%','padding':20, 'display': 'inline-block'}),
         
         html.Div([
             dcc.Dropdown(
                 id='yaxis-column1',
                 options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Value added, gross'
+                value='Imports of goods'
             ),
             dcc.RadioItems(
                 id='yaxis-type1',
@@ -69,7 +84,7 @@ app.layout = html.Div([
                 value='Current prices, million euro',
                 labelStyle={'display': 'inline-block'}
             )
-        ],style={'width': '40%','padding':20,'float': 'right', 'display': 'inline-block'})
+        ],style={'width': '45%','padding':20,'float': 'right', 'display': 'inline-block'})
     ]),
 
     dcc.Graph(id='indicator-graphic'),
@@ -84,30 +99,43 @@ app.layout = html.Div([
     ),
     
     #Second graph
+        
+    html.H4('Graph 2: Line chart', 
+            style={
+            'color': 'hotpink'
+        } ),
+    
     html.Div([
-
+        
         html.Div([
             dcc.Dropdown(
                 id='xaxis-column2',
                 options=[{'label': i, 'value': i} for i in available_geo],
                 value='Spain'
-            ),
+            )
                     
         ],
-        style={'width': '40%', 'padding':30, 'display': 'inline-block'}),
+        style={'width': '35%', 'padding':30, 'display': 'inline-block'}),
         
         html.Div([
             dcc.Dropdown(
                 id='yaxis-column2',
                 options=[{'label': i, 'value': i} for i in available_indicators],
                 value='Gross domestic product at market prices'
+            ),
+            dcc.RadioItems(
+                id='yaxis-type2',
+                options=[{'label': i, 'value': i} for i in available_unit],
+                value='Current prices, million euro',
+                labelStyle={'display': 'inline-block'}
             )
-        ],style={'width': '40%','padding':30, 'float': 'right', 'display': 'inline-block'})
+        ],
+        style={'width': '55%','padding':30, 'float': 'right', 'display': 'inline-block'})    
     ]),
 
     dcc.Graph(id='indicator-graphic2'),
-
 ])
+
 
 @app.callback(
     dash.dependencies.Output('indicator-graphic', 'figure'),
@@ -119,42 +147,47 @@ app.layout = html.Div([
 def update_graph(xaxis_column_name1, yaxis_column_name1,
                  xaxis_type1, yaxis_type1,
                  year_value1):
-    dfo = df[df['TIME'] == year_value1]
-    dfx = dfo[dfo['UNIT'] == xaxis_type1]
-    dfy = dfo[dfo['UNIT'] == yaxis_type1]
+    dfx = df[(df['TIME'] == year_value1)&(df['UNIT'] == xaxis_type1)]
+    dfy = df[(df['TIME'] == year_value1)&(df['UNIT'] == yaxis_type1)]
     
     return {
         'data': [go.Scatter(
-            x=dfx[dfx['NA_ITEM'] == xaxis_column_name1]['Value'],
-            y=dfy[dfy['NA_ITEM'] == yaxis_column_name1]['Value'],
-            text=dfy[dfy['NA_ITEM'] == yaxis_column_name1]['GEO'], 
+            x=dfx[(dfx['NA_ITEM'] == xaxis_column_name1) & (dfx['GEO']==i)]['Value'],
+            y=dfy[(dfy['NA_ITEM'] == yaxis_column_name1) & (dfy['GEO']==i)]['Value'],
+            text=i, 
             mode='markers',
             marker={
                 'size': 15,
                 'opacity': 0.5,
                 'line': {'width': 0.5, 'color': 'white'}
-            }
-        )],
+            }, name=i ) for i in available_geo
+        ],
         'layout': go.Layout(
             xaxis={
                 'title': xaxis_column_name1,
-                'type': 'log'
+                'type': 'linear',
+                'color':'darkviolet',
             },
             yaxis={
                 'title': yaxis_column_name1,
-                'type': 'log'
+                'type': 'linear',
+                'color':'darkviolet'
             },
             margin={'l': 60, 'b': 60, 't': 10, 'r': 30},
-            hovermode='closest'
+            hovermode='closest',
+            plot_bgcolor='#F5E6F6',
+            paper_bgcolor= '#F5E6F6',
+            font={'color': 'darkviolet'}
         )
     }
 
 @app.callback(
     dash.dependencies.Output('indicator-graphic2', 'figure'),
     [dash.dependencies.Input('xaxis-column2', 'value'),
-     dash.dependencies.Input('yaxis-column2', 'value')])
-def update_graph(xaxis_column_name2, yaxis_column_name2):
-    dfz=dff[dff['GEO']==xaxis_column_name2]    
+     dash.dependencies.Input('yaxis-column2', 'value'),
+     dash.dependencies.Input('yaxis-type2', 'value')])
+def update_graph(xaxis_column_name2, yaxis_column_name2,yaxis_type2):
+    dfz=df[(df['GEO']==xaxis_column_name2)&(df['UNIT']==yaxis_type2)]
     
     return {
         'data': [go.Scatter(
@@ -162,26 +195,27 @@ def update_graph(xaxis_column_name2, yaxis_column_name2):
             y=dfz[dfz['NA_ITEM'] == yaxis_column_name2]['Value'],
             text=available_year, 
             mode='lines',
-            marker={
-                'size': 15,
-                'opacity': 1.0,
-                'line': {'width': 0.5, 'color': 'white'}
-            }
+            line={'width': 2.5, 'color': 'hotpink'}
         )],
         'layout': go.Layout(
             xaxis={
                 'title': 'Year',
-                'type': 'date'
+                'type': 'date',
+                'linecolor':'hotpink',
+                'color':'hotpink'
             },
             yaxis={
                 'title': yaxis_column_name2,
-                'type': 'linear'
+                'type': 'linear',
+                'linecolor':'hotpink',
+                'color':'hotpink'
             },
             margin={'l': 60, 'b': 60, 't': 10, 'r': 30},
-            hovermode='closest'
+            hovermode='closest',
+            plot_bgcolor='#F5E6F6',
+            paper_bgcolor= '#F5E6F6',
         )
     }
 
 if __name__ == '__main__':
-    app.run_server()   
-
+    app.run_server()
